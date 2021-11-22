156224fe6c3f86e8d8a78cc726e735c04cfcf869
Dominik Dominik <nostrada.muss@wp.pl>
2021-11-23 00:27:19 +0100

    Add files via upload

---
+++ Zadanie 6/main/java/android/mobile/fragmentylistyadaptery/TaskFragment.java
@@ -0,0 +1,73 @@
+package android.mobile.fragmentylistyadaptery;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import java.util.UUID;
+
+public class TaskFragment extends Fragment {
+    private Task task = null;
+    public static String ARG_TASK_ID = "task_id";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        UUID taskId = (UUID) getArguments().getSerializable(ARG_TASK_ID);
+        task = TaskStorage.getInstance().getTask(taskId);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_task, container, false);
+        EditText nameField = view.findViewById(R.id.task_name);
+        nameField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                task.setName(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+        Button dateButton = view.findViewById(R.id.task_date);
+        dateButton.setText(task.getDate().toString());
+        dateButton.setEnabled(false);
+
+        CheckBox doneCheckBox = view.findViewById(R.id.task_done);
+        doneCheckBox.setChecked(task.isDone());
+        doneCheckBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            task.setDone(isChecked);
+        });
+
+        nameField.setText(task.getName());
+        dateButton.setText(task.getDate().toString());
+        doneCheckBox.setChecked(task.isDone());
+
+        return view;
+    }
+
+    public static TaskFragment newInstance(UUID taskId)
+    {
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(ARG_TASK_ID, taskId);
+        TaskFragment taskFragment = new TaskFragment();
+        taskFragment.setArguments(bundle);
+        return taskFragment;
+    }
+}
